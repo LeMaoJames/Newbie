d64f1e2007646a69c8b6d9df78f89900db9b507b
LeMao <152803601+LeMaoJames@users.noreply.github.com>
2024-01-05 13:39:17 +0700

    Update BaitapBST.c

--- BaitapBST.c
+++ BaitapBST.c
@@ -1,3 +1,23 @@
+// Một trung tâm mua sắm trực tuyến có n mặt hàng, trong bài tập này đối với mỗi mặt hàng chúng ta chỉ quan tâm đến mã số và trọng lượng của mặt hàng. Mã số có không quá 9 ký tự, còn trọng lượng là số nguyên (được tính bằng gam). Các đơn hàng được tạo liên tục và cần tính tổng trọng lượng các sản phẩm trong đơn hàng.
+// Dữ liệu đầu vào được cho như sau:
+// Đầu tiên là số n (<100 000) là số lượng mặt hàng.
+// Sau đó là n dòng, mỗi dòng chứa 1 cặp mã số và trọng lượng của mặt hàng tương ứng.
+// Tiếp đó là số m (< 10000) - số lượng đơn hàng.
+// Cuối cùng là m dòng mỗi dòng mô tả 1 đơn hàng bao gồm: đầu tiên là số k - số lượng mục hàng trong đơn hàng, sau đó là k cặp gồm mã số sản phẩm và số lượng sản phẩm.
+// Dữ liệu đầu ra:
+// Chương trình xuất ra 1 số nguyên là trọng lượng tương ứng với mỗi đơn hàng.
+// Ví dụ,
+// Đầu vào:
+// 3
+// a11 150
+// a12 500
+// a20 200
+// 2
+// 1 a11 2
+// 2 a12 1 a20 3
+// Đầu ra:
+// 300
+// 1100
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
